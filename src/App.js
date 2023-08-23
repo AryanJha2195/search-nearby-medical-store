4d2ed3a00cc1fbbe3fb999ae0572491df8626ed5
Aryan Jha <aryan2195jha@gmail.com>
2023-08-23 15:52:41 +0530

    All Changes yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarr from './Component/NavBarr';
+import PostNavBar from './Component/PostNavBar.jsx';
+import Slider from './Component/Slider.jsx';
+import Cards from './Component/Cards.jsx';
+import PostCards from './Component/PostCards.jsx';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <NavBarr />
+      <PostNavBar />
+      <Slider />
+      <Cards />
+      <h3>Trending Today</h3>
+      <PostCards />
+
+      {/* Add the Google Map component */}
+      <Map
+        google={window.google}
+        zoom={14}
+        initialCenter={{ lat: 37.7749, lng: -122.4194 }} // Change this to your desired initial location
+      />
     </div>
   );
 }
 
-export default App;
+// Wrap your App component with the GoogleApiWrapper, providing your API key
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC7Kdvc6_JHHG7EWZYweHzvYQWEDe1bUsM'
+})(App);
